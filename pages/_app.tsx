ae64ef3c6dd4694e843e51acd894e55b861b1de0
Clifton King <cliftonk@gmail.com>
2021-01-17 19:32:53 -0600

    move root provider to _app from index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { NextComponentType, NextPageContext } from 'next';
 import type { NextRouter } from 'next/router';
 import { FunctionComponent } from 'react';
+import { RootProvider } from '../components/RootProvider';
 
 export interface AppRenderProps {
   pageProps: Record<string, unknown>;
@@ -10,11 +11,20 @@ export interface AppRenderProps {
     AppRenderProps,
     Record<string, unknown>
   >;
+  cookies: string;
   router: NextRouter;
 }
 
-const App: FunctionComponent<AppRenderProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+const App: FunctionComponent<AppRenderProps> = ({
+  Component,
+  pageProps,
+  cookies,
+}) => (
+  <RootProvider cookies={cookies}>
+    <Component {...pageProps} />
+  </RootProvider>
 );
 
 export default App;
+
+export { getServerSideProps } from '../components/RootProvider';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,37 @@
 import { Box, Code, Text, Link, VStack, Grid } from '@chakra-ui/react';
 import { FunctionComponent } from 'react';
-import { RootProvider } from '../components/RootProvider';
 import { Layout } from '../components/Layout';
 import { Logo } from '../components/Logo';
 import { NextChakraLink } from '../components/NextChakraLink';
 
-interface IndexProps {
-  cookies?: string;
-}
-
-const IndexPage: FunctionComponent<IndexProps> = ({ cookies }) => (
-  <RootProvider cookies={cookies}>
-    <Layout title="Next.js + TypeScript example">
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">pages/index.tsx</Code> and save to
-              reload.
-              <br />
-              <br />
-              <NextChakraLink href="/properties" color="teal.500">
-                View the properties
-              </NextChakraLink>{' '}
-              to see the Nextjs <Code fontSize="xl">&lt;Link&gt;</Code> in
-              action
-            </Text>
-            <Link
-              color="teal.500"
-              fontSize="2xl"
-              href="https://chakra-ui.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn more about Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </Layout>
-  </RootProvider>
+const IndexPage: FunctionComponent = () => (
+  <Layout title="Next.js + TypeScript example">
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <VStack spacing={8}>
+          <Logo h="40vmin" pointerEvents="none" />
+          <Text>
+            Edit <Code fontSize="xl">pages/index.tsx</Code> and save to reload.
+            <br />
+            <br />
+            <NextChakraLink href="/properties" color="teal.500">
+              View the properties
+            </NextChakraLink>{' '}
+            to see the Nextjs <Code fontSize="xl">&lt;Link&gt;</Code> in action
+          </Text>
+          <Link
+            color="teal.500"
+            fontSize="2xl"
+            href="https://chakra-ui.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn more about Chakra
+          </Link>
+        </VStack>
+      </Grid>
+    </Box>
+  </Layout>
 );
 
 export default IndexPage;
-export { getServerSideProps } from '../components/RootProvider';
