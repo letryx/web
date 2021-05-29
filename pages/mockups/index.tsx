390ca88ca91157e2486bf35faad0262f47dce91c
Clifton King <cliftonk@gmail.com>
2021-05-29 15:52:37 -0500

    add indemnity dashboard link to /mockups

--- pages/mockups/index.tsx
+++ pages/mockups/index.tsx
@@ -1,26 +1,35 @@
-import { Container, Stack, Text } from '@chakra-ui/react';
+import { Container, Stack, Text, Link } from '@chakra-ui/react';
 import { FC } from 'react';
 import { Layout } from 'components/layout';
 import { NextChakraLink } from 'components/next-chakra-link';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const MockupLink: FC<{ name: string; page: string }> = ({
   name,
   page,
   children,
-}) => (
-  <NextChakraLink
-    href={`/mockups/${page}`}
-    boxShadow="md"
-    _hover={{ boxShadow: 'lg', textDecoration: 'underline' }}
-    textAlign="left"
-    p={4}
-  >
-    <Text>{name}</Text>
-    <Text fontSize="sm" style={{ display: 'inline-block' }}>
-      {children}
-    </Text>
-  </NextChakraLink>
-);
+}) => {
+  const isExternal = page.substr(0, 4) === 'http';
+  const LinkType = isExternal ? Link : NextChakraLink;
+  return (
+    <LinkType
+      isExternal={isExternal}
+      href={isExternal ? page : `/mockups/${page}`}
+      boxShadow="md"
+      _hover={{ boxShadow: 'lg', textDecoration: 'underline' }}
+      textAlign="left"
+      p={4}
+    >
+      <Text>
+        {name}
+        {isExternal && <ExternalLinkIcon mx={2} pb={1.5} />}
+      </Text>
+      <Text fontSize="sm" style={{ display: 'inline-block' }}>
+        {children}
+      </Text>
+    </LinkType>
+  );
+};
 
 const mockup: FC = () => (
   <Layout title="Letryx">
@@ -30,6 +39,13 @@ const mockup: FC = () => (
         <MockupLink name="Permitted Encumbrances" page="permitted-encumbrances">
           A qualitative analysis of a contract clause with regards to common
           inclusions/exceptions and buyer vs seller friendliness.
+        </MockupLink>
+        <MockupLink
+          name="Indemnity Deductible"
+          page="https://web-scatter-mockup.vercel.app"
+        >
+          A quantitative analysis of indemnity terms from a corpus of similar
+          contracts.
         </MockupLink>
       </Stack>
     </Container>
