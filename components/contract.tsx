d46839df122a3ada45df541233f0c315e882e4b6
Clifton King <cliftonk@gmail.com>
2021-08-24 21:38:26 -0500

    refactor contract show logic to own component

---
+++ components/contract.tsx
@@ -0,0 +1,226 @@
+import {
+  Box,
+  Button,
+  ListItem,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Skeleton,
+  SkeletonText,
+  Text,
+  useBreakpointValue,
+  useColorModeValue,
+  useDisclosure,
+} from '@chakra-ui/react';
+import createDOMPurify from 'dompurify';
+import parse from 'html-react-parser';
+import {
+  SearchResultFragment,
+  useGetSecContractQuery,
+} from 'lib/generated/graphql/apollo-schema';
+import { FC, HTMLAttributes, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+import { RemoveScroll } from 'react-remove-scroll';
+
+const isSSR = typeof window === 'undefined';
+let domPurify: createDOMPurify.DOMPurifyI | null;
+
+if (!isSSR) {
+  domPurify = createDOMPurify(window);
+}
+
+interface IFrameProps extends HTMLAttributes<HTMLElement> {
+  allow?: string;
+  allowFullScreen?: boolean;
+  allowTransparency?: boolean;
+  frameBorder?: number | string;
+  height?: number | string;
+  marginHeight?: number;
+  marginWidth?: number;
+  name?: string;
+  sandbox?: string;
+  scrolling?: string;
+  seamless?: boolean;
+  src?: string;
+  srcDoc?: string;
+  width?: number | string;
+}
+
+export const FunctionalIFrameComponent: FC<IFrameProps> = ({
+  children,
+  title,
+  ...props
+}) => {
+  const [contentRef, setContentRef] = useState<HTMLIFrameElement | null>(null);
+  const [paddingRight, setPaddingRight] = useState(0);
+  const [height, setHeight] = useState(500);
+  const mountNode = contentRef?.contentWindow?.document?.body;
+  const { scrollHeight, clientHeight, scrollWidth, clientWidth } =
+    mountNode || {};
+  const textColor = useColorModeValue('black', 'white');
+
+  useEffect(() => {
+    // handle negative margin in docs
+    if (!mountNode) return;
+    if (mountNode.scrollWidth > mountNode.clientWidth) {
+      setPaddingRight(mountNode.scrollWidth - mountNode.clientWidth + 30);
+    }
+    setHeight(mountNode.scrollHeight);
+  }, [mountNode, scrollHeight, clientHeight, scrollWidth, clientWidth]);
+  mountNode?.setAttribute(
+    'style',
+    `padding-right: ${paddingRight}px; overflow-y: hidden;`
+  );
+  if (contentRef?.contentWindow?.document) {
+    const doc = contentRef?.contentWindow?.document;
+    const style_el = doc.createElement('style');
+    style_el.innerText = `
+      p,font,span,tr,td {
+        color: ${textColor} !important;
+        border-color: ${textColor} !important;
+        background-color: transparent !important;
+      }
+    `;
+    mountNode?.appendChild(style_el);
+  }
+
+  return (
+    <>
+      <iframe
+        height={`${height}px`}
+        title={title}
+        {...{ ...props }}
+        ref={setContentRef}
+      >
+        {mountNode && createPortal(children, mountNode)}
+      </iframe>
+    </>
+  );
+};
+
+const stringToDOM = (rawHtml: string | undefined): JSX.Element[] => {
+  if (!domPurify || !rawHtml) return [];
+  const html = domPurify.sanitize(rawHtml, {
+    // KEEP_CONTENT: false,
+    IN_PLACE: true,
+  });
+  const parsedHtml = parse(html.replaceAll(/\bPAGEBREAK\b/gi, ''));
+  return (Array.isArray(parsedHtml) ? parsedHtml : [parsedHtml]).map((el) =>
+    typeof el === 'string' && el.trim() ? <pre>{el}</pre> : <>{el}</>
+  );
+};
+
+interface ContractIFrameProps extends SearchResultFragment {
+  dom: JSX.Element[];
+}
+
+export const ContractIFrame: FC<ContractIFrameProps> = ({
+  dom,
+  accession_number,
+  sequence,
+}) => {
+  const fontSize =
+    useBreakpointValue(['90%', '100%', '110%', '120%']) || '100%';
+  const color = useColorModeValue('black', 'white');
+
+  return (
+    <FunctionalIFrameComponent
+      title={`contract-${accession_number}-${sequence}`}
+      width="100%"
+    >
+      <RemoveScroll forwardProps noIsolation>
+        <>
+          <Box
+            style={{
+              color,
+              fontSize,
+            }}
+          >
+            {dom}
+          </Box>
+        </>
+      </RemoveScroll>
+    </FunctionalIFrameComponent>
+  );
+};
+
+export const ContractModal: FC<SearchResultFragment> = (contract) => {
+  const { accession_number, sequence } = contract;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { data, loading } = useGetSecContractQuery({
+    variables: { accession_number, sequence },
+    skip: isSSR || !isOpen,
+  });
+
+  const { description, sec_filing, attachment_type } =
+    data?.sec_filing_attachment_by_pk || {};
+  const { filing_date, filing_type, sec_company } = sec_filing || {};
+  const { name: companyName } = sec_company || {};
+  const dom = stringToDOM(data?.sec_filing_attachment_by_pk?.contents);
+
+  return (
+    <>
+      <Button onClick={onOpen}>View</Button>
+      <Modal isOpen={isOpen} onClose={onClose} blockScrollOnMount={false}>
+        <ModalOverlay />
+        <ModalContent width="95vw" maxWidth="900px">
+          <ModalHeader>
+            {loading ? (
+              <Skeleton>{'x'.repeat(20)}</Skeleton>
+            ) : (
+              description && (
+                <Text as="span" mr={3}>
+                  {description}
+                </Text>
+              )
+            )}
+            <Text as="span" fontSize="80%" fontWeight="normal">
+              ({filing_type} - {attachment_type})
+            </Text>
+            <Text fontSize="80%" fontWeight="normal">
+              Filed by {companyName} on {filing_date?.toString()}
+            </Text>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody px={[2, 4, 6]}>
+            <SkeletonText isLoaded={!loading} noOfLines={30}>
+              <ContractIFrame {...{ dom, ...contract }} />
+            </SkeletonText>
+          </ModalBody>
+          <ModalFooter>
+            <Button mr={3} onClick={onClose}>
+              Close
+            </Button>
+            {/* <Button variant="ghost">Secondary Action</Button> */}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export const ContractSnippet: FC<SearchResultFragment> = (contractProps) => {
+  const {
+    company_name,
+    filing_type,
+    description,
+    attachment_type,
+    filing_date,
+  } = contractProps;
+  return (
+    <ListItem>
+      <Box>
+        {filing_date}
+        {company_name}
+        {filing_type}
+        {attachment_type}
+        {description}
+        <ContractModal {...contractProps} />
+      </Box>
+    </ListItem>
+  );
+};
--- pages/contracts/index.tsx
+++ pages/contracts/index.tsx
@@ -2,7 +2,6 @@ import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import {
   Box,
   BoxProps,
-  Button,
   Checkbox,
   CheckboxGroup,
   Flex,
@@ -17,53 +16,20 @@ import {
   InputProps,
   InputRightAddon,
   List,
-  ListItem,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
   Skeleton,
-  SkeletonText,
   Spacer,
   Stack,
   Text,
   Tooltip,
-  useBreakpointValue,
   useColorModeValue,
-  useDisclosure,
   VStack,
 } from '@chakra-ui/react';
 import { SingleDatepicker } from 'chakra-dayzed-datepicker';
+import { ContractSnippet } from 'components/contract';
 import { Layout } from 'components/layout';
-import createDOMPurify from 'dompurify';
-import parse from 'html-react-parser';
-import {
-  SearchResultFragment,
-  useGetSecContractQuery,
-  useSearchSecContractsQuery,
-} from 'lib/generated/graphql/apollo-schema';
-import {
-  Dispatch,
-  FC,
-  HTMLAttributes,
-  SetStateAction,
-  useEffect,
-  useState,
-} from 'react';
-import { createPortal } from 'react-dom';
+import { useSearchSecContractsQuery } from 'lib/generated/graphql/apollo-schema';
+import { Dispatch, FC, SetStateAction, useState } from 'react';
 import { MdFilterList, MdSearch } from 'react-icons/md';
-import { RemoveScroll } from 'react-remove-scroll';
-
-let domPurify: createDOMPurify.DOMPurifyI | null;
-
-const isSSR = typeof window === 'undefined';
-
-if (!isSSR) {
-  domPurify = createDOMPurify(window);
-}
 
 type SearchBarProps = InputProps & {
   contractCount?: number;
@@ -102,198 +68,6 @@ const SearchBar: FC<SearchBarProps> = ({
     </InputRightAddon>
   </InputGroup>
 );
-
-interface IFrameProps extends HTMLAttributes<HTMLElement> {
-  allow?: string;
-  allowFullScreen?: boolean;
-  allowTransparency?: boolean;
-  frameBorder?: number | string;
-  height?: number | string;
-  marginHeight?: number;
-  marginWidth?: number;
-  name?: string;
-  sandbox?: string;
-  scrolling?: string;
-  seamless?: boolean;
-  src?: string;
-  srcDoc?: string;
-  width?: number | string;
-}
-
-export const FunctionalIFrameComponent: FC<IFrameProps> = ({
-  children,
-  title,
-  ...props
-}) => {
-  const [contentRef, setContentRef] = useState<HTMLIFrameElement | null>(null);
-  const [paddingRight, setPaddingRight] = useState(0);
-  const [height, setHeight] = useState(500);
-  const mountNode = contentRef?.contentWindow?.document?.body;
-  const { scrollHeight, clientHeight, scrollWidth, clientWidth } =
-    mountNode || {};
-  const textColor = useColorModeValue('black', 'white');
-
-  useEffect(() => {
-    // handle negative margin in docs
-    if (!mountNode) return;
-    if (mountNode.scrollWidth > mountNode.clientWidth) {
-      setPaddingRight(mountNode.scrollWidth - mountNode.clientWidth + 30);
-    }
-    setHeight(mountNode.scrollHeight);
-  }, [mountNode, scrollHeight, clientHeight, scrollWidth, clientWidth]);
-  mountNode?.setAttribute(
-    'style',
-    `padding-right: ${paddingRight}px; overflow-y: hidden;`
-  );
-  if (contentRef?.contentWindow?.document) {
-    const doc = contentRef?.contentWindow?.document;
-    const style_el = doc.createElement('style');
-    style_el.innerText = `
-      p,font,span,tr,td {
-        color: ${textColor} !important;
-        border-color: ${textColor} !important;
-        background-color: transparent !important;
-      }
-    `;
-    mountNode?.appendChild(style_el);
-  }
-
-  return (
-    <>
-      <iframe
-        height={`${height}px`}
-        title={title}
-        {...{ ...props }}
-        ref={setContentRef}
-      >
-        {mountNode && createPortal(children, mountNode)}
-      </iframe>
-    </>
-  );
-};
-
-const stringToDOM = (rawHtml: string | undefined): JSX.Element[] => {
-  if (!domPurify || !rawHtml) return [];
-  const html = domPurify.sanitize(rawHtml, {
-    // KEEP_CONTENT: false,
-    IN_PLACE: true,
-  });
-  const parsedHtml = parse(html.replaceAll(/\bPAGEBREAK\b/gi, ''));
-  return (Array.isArray(parsedHtml) ? parsedHtml : [parsedHtml]).map((el) =>
-    typeof el === 'string' && el.trim() ? <pre>{el}</pre> : <>{el}</>
-  );
-};
-
-interface ContractIFrameProps extends SearchResultFragment {
-  dom: JSX.Element[];
-}
-
-const ContractIFrame: FC<ContractIFrameProps> = ({
-  dom,
-  accession_number,
-  sequence,
-}) => {
-  const fontSize =
-    useBreakpointValue(['90%', '100%', '110%', '120%']) || '100%';
-  const color = useColorModeValue('black', 'white');
-
-  return (
-    <FunctionalIFrameComponent
-      title={`contract-${accession_number}-${sequence}`}
-      width="100%"
-    >
-      <RemoveScroll forwardProps noIsolation>
-        <>
-          <Box
-            style={{
-              color,
-              fontSize,
-            }}
-          >
-            {dom}
-          </Box>
-        </>
-      </RemoveScroll>
-    </FunctionalIFrameComponent>
-  );
-};
-
-const ContractModal: FC<SearchResultFragment> = (contract) => {
-  const { accession_number, sequence } = contract;
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const { data, loading } = useGetSecContractQuery({
-    variables: { accession_number, sequence },
-    skip: isSSR || !isOpen,
-  });
-
-  const { description, sec_filing, attachment_type } =
-    data?.sec_filing_attachment_by_pk || {};
-  const { filing_date, filing_type, sec_company } = sec_filing || {};
-  const { name: companyName } = sec_company || {};
-  const dom = stringToDOM(data?.sec_filing_attachment_by_pk?.contents);
-
-  return (
-    <>
-      <Button onClick={onOpen}>View</Button>
-      <Modal isOpen={isOpen} onClose={onClose} blockScrollOnMount={false}>
-        <ModalOverlay />
-        <ModalContent width="95vw" maxWidth="900px">
-          <ModalHeader>
-            {loading ? (
-              <Skeleton>{'x'.repeat(20)}</Skeleton>
-            ) : (
-              description && (
-                <Text as="span" mr={3}>
-                  {description}
-                </Text>
-              )
-            )}
-            <Text as="span" fontSize="80%" fontWeight="normal">
-              ({filing_type} - {attachment_type})
-            </Text>
-            <Text fontSize="80%" fontWeight="normal">
-              Filed by {companyName} on {filing_date?.toString()}
-            </Text>
-          </ModalHeader>
-          <ModalCloseButton />
-          <ModalBody px={[2, 4, 6]}>
-            <SkeletonText isLoaded={!loading} noOfLines={30}>
-              <ContractIFrame {...{ dom, ...contract }} />
-            </SkeletonText>
-          </ModalBody>
-          <ModalFooter>
-            <Button mr={3} onClick={onClose}>
-              Close
-            </Button>
-            {/* <Button variant="ghost">Secondary Action</Button> */}
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </>
-  );
-};
-
-const ContractSnippet: FC<SearchResultFragment> = (contractProps) => {
-  const {
-    company_name,
-    filing_type,
-    description,
-    attachment_type,
-    filing_date,
-  } = contractProps;
-  return (
-    <ListItem>
-      <Box>
-        {filing_date}
-        {company_name}
-        {filing_type}
-        {attachment_type}
-        {description}
-        <ContractModal {...contractProps} />
-      </Box>
-    </ListItem>
-  );
-};
 
 type FilterProps = BoxProps & {
   companyCount?: number;
