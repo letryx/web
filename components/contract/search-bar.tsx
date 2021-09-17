c08da94924d0f80a91fc636bd91dba3d23d51094
Clifton King <cliftonk@gmail.com>
2021-09-17 02:58:52 -0500

    add query modal

--- components/contract/search-bar.tsx
+++ components/contract/search-bar.tsx
@@ -14,10 +14,12 @@ import {
   Skeleton,
   Text,
   useColorModeValue,
+  useDisclosure,
 } from '@chakra-ui/react';
+import { QueryModal } from 'components/contract/query-modal';
 import { Dispatch, FC, SetStateAction } from 'react';
 import { FaPlusCircle } from 'react-icons/fa';
-import { GrClear } from 'react-icons/gr';
+import { GrChat, GrClear } from 'react-icons/gr';
 import { MdSearch } from 'react-icons/md';
 
 type SearchBarProps = InputProps & {
@@ -40,63 +42,73 @@ export const SearchBar: FC<SearchBarProps> = ({
   compSetSize,
   clearCompSet,
   ...props
-}) => (
-  <InputGroup>
-    <InputLeftElement>
-      <MdSearch fontSize="1.5rem" />
-    </InputLeftElement>
-    <Input
-      type="text"
-      autoFocus
-      autoCapitalize="off"
-      autoCorrect="off"
-      autoComplete="off"
-      placeholder={placeholder}
-      aria-label={placeholder}
-      _placeholder={{ color: useColorModeValue('gray.600', 'gray.200') }}
-      onChange={(e) => setValue(e.target.value)}
-      {...props}
-    />
-    <InputRightAddon textAlign="right">
-      <Text>
-        {contractCount ? (
-          contractCount.toLocaleString()
-        ) : (
-          <Skeleton minWidth="3rem" isLoaded={!isLoading} mr={2}>
-            99,999
-          </Skeleton>
-        )}
-      </Text>
-    </InputRightAddon>
-    <Button
-      ml="2"
-      colorScheme="messenger"
-      onClick={onClickAdd}
-      isLoading={addIsLoading}
-      leftIcon={<FaPlusCircle />}
-      textAlign="center"
-      width="70px"
-      minWidth="70px"
-    >
-      All
-    </Button>
-    {compSetSize > 0 && (
-      <Menu isLazy>
-        <MenuButton
-          as={Button}
-          ml="2"
-          colorScheme="green"
-          rightIcon={<Icon as={ChevronDownIcon} />}
-          minWidth="150px"
-        >
-          <Text>{compSetSize.toLocaleString()} Comps</Text>
-        </MenuButton>
-        <MenuList textAlign="left">
-          <MenuItem onClick={clearCompSet} icon={<GrClear />}>
-            Clear
-          </MenuItem>
-        </MenuList>
-      </Menu>
-    )}
-  </InputGroup>
-);
+}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <InputGroup>
+      <InputLeftElement>
+        <MdSearch fontSize="1.5rem" />
+      </InputLeftElement>
+      <Input
+        type="text"
+        autoFocus
+        autoCapitalize="off"
+        autoCorrect="off"
+        autoComplete="off"
+        placeholder={placeholder}
+        aria-label={placeholder}
+        _placeholder={{ color: useColorModeValue('gray.600', 'gray.200') }}
+        onChange={(e) => setValue(e.target.value)}
+        {...props}
+      />
+      <InputRightAddon textAlign="right">
+        <Text>
+          {contractCount ? (
+            contractCount.toLocaleString()
+          ) : (
+            <Skeleton minWidth="3rem" isLoaded={!isLoading} mr={2}>
+              99,999
+            </Skeleton>
+          )}
+        </Text>
+      </InputRightAddon>
+      <Button
+        ml="2"
+        colorScheme="messenger"
+        onClick={onClickAdd}
+        isLoading={addIsLoading}
+        leftIcon={<FaPlusCircle />}
+        textAlign="center"
+        width="70px"
+        minWidth="70px"
+      >
+        All
+      </Button>
+      {compSetSize > 0 && (
+        <Menu isLazy>
+          <MenuButton
+            as={Button}
+            ml="2"
+            colorScheme="green"
+            rightIcon={<Icon as={ChevronDownIcon} />}
+            minWidth="150px"
+          >
+            <Text>
+              {compSetSize.toLocaleString()} Comp{compSetSize > 1 && 's'}
+            </Text>
+          </MenuButton>
+          <MenuList textAlign="left">
+            <MenuItem onClick={clearCompSet} icon={<GrClear />}>
+              Clear
+            </MenuItem>
+            <MenuItem icon={<Icon as={GrChat} />} onClick={onOpen}>
+              Submit Query
+            </MenuItem>
+          </MenuList>
+          <QueryModal {...{ compSetSize, isOpen, onClose }} />
+        </Menu>
+      )}
+    </InputGroup>
+  );
+};
