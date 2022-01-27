9de6389be5a1c05e041e6e90a0a31adbc879181f
Clifton King <cliftonk@gmail.com>
2022-01-27 00:05:04 -0600

    take accessToken via queryParam, fix streams

--- src/pages/api/contracts/export-pdf/[slug].ts
+++ src/pages/api/contracts/export-pdf/[slug].ts
@@ -1,5 +1,4 @@
 /* eslint-disable no-console */
-import { getAccessToken } from '@auth0/nextjs-auth0';
 import { createApiApolloClient } from 'lib/apollo-client';
 import {
   GetSecContractDocument,
@@ -9,22 +8,26 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'node-fetch';
 
 export default async (
-  req: NextApiRequest,
-  res: NextApiResponse<Uint8Array>
+  { query, method }: NextApiRequest,
+  res: NextApiResponse
 ): Promise<void> => {
-  if (req.method !== 'GET') throw new Error('only accepts GET');
-  const slug =
-    typeof req.query.slug === 'object'
-      ? req.query.slug[0]
-      : req.query.slug || '';
-  const uid = slug.split(/-|\./)[0];
+  if (method !== 'GET') throw new Error('only accepts GET');
 
-  const { accessToken } = await getAccessToken(req, res);
+  const accessToken =
+    typeof query.accessToken === 'object'
+      ? query.accessToken[0]
+      : query.accessToken;
+
   if (!accessToken) {
     res.status(401);
     res.end();
     return;
   }
+
+  const slug =
+    typeof query.slug === 'object' ? query.slug[0] : query.slug || '';
+  const uid = slug.split(/-|\./)[0];
+
   const client = createApiApolloClient(accessToken);
   const contract = await client.query<GetSecContractQuery>({
     query: GetSecContractDocument,
@@ -54,18 +57,14 @@ export default async (
   if (!pdfRes.ok) throw new Error(`unexpected response ${pdfRes.statusText}`);
   if (!pdfBody) throw new Error('empty response body');
 
-  res.status(200);
   res.setHeader('Content-Type', 'application/pdf');
   res.setHeader('Content-Disposition', 'attachment; filename=export.pdf');
+  res.status(200);
 
   await new Promise<void>((resolve, reject) => {
     pdfBody
-      .on('error', (reason: string) => reject(reason))
-      .on('data', (chunk: Uint8Array) => res.send(chunk))
-      .on('end', () => {
-        resolve();
-      });
+      .pipe(res)
+      .on('finish', () => resolve())
+      .on('error', (err) => reject(err));
   });
-
-  res.end();
 };
